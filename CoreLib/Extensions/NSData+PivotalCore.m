03082ebf4f1ede4015276fcbb0098ce660981f5a
Adam Milligan & JB Steadman <pair+am+jb@pivotallabs.com>
2010-07-07 16:29:04 -0700

    Static library for core kit.

--- CoreLib/Extensions/NSData+PivotalCore.m
+++ CoreLib/Extensions/NSData+PivotalCore.m
@@ -1,4 +1,5 @@
 #import <CommonCrypto/CommonDigest.h>
+#import <Foundation/Foundation.h>
 #import "NSData+PivotalCore.h"
 
 @implementation NSData (PivotalCore)
--- CoreLib/Extensions/NSString+PivotalCore.m
+++ CoreLib/Extensions/NSString+PivotalCore.m
@@ -1,3 +1,4 @@
+#import <Foundation/Foundation.h>
 #import "NSString+PivotalCore.h"
 
 static const unsigned char BASE64_DICTIONARY[] = {
