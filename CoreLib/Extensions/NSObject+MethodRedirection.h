a0690720a960f25db5e8800e2d177276cf0d5e14
Onsi Fakhouri <pair+onsi@pivotallabs.com>
2012-01-10 10:40:33 -0800

    Added support to redirect instance and class methods with arbitrary arguments (NSObject+MethodRedirection).
    This is useful for rewriting methods in categories without losing the original implementation.
    
    Reimplemented NSObject+MethodDecoration (a specific use-case of the more general MethodRedirection methods) to user NSObject+MethodRedirection.

---
+++ CoreLib/Extensions/NSObject+MethodRedirection.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (MethodRedirection)
+
++ (void)redirectSelector:(SEL)originalSelector to:(SEL)newSelector andRenameItTo:(SEL)renamedSelector;
++ (void)redirectClassSelector:(SEL)originalSelector to:(SEL)newSelector andRenameItTo:(SEL)renamedSelector;
+
++ (void)redirectSelector:(SEL)originalSelector forClass:(Class)klass to:(SEL)newSelector andRenameItTo:(SEL)renamedSelector;
+
+@end
