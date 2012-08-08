76e0faba68dc95e66b4e0d311fb4cc5eefcd4e26
Andrew Kitchen & Tyler Schultz <pair+akitchen+tyler@pivotallabs.com>
2012-08-08 11:53:43 -0700

    Added test tap handler support for optional UITableViewDelegate method

--- SpecHelperLib/Extensions/UITableViewCell+Spec.m
+++ SpecHelperLib/Extensions/UITableViewCell+Spec.m
@@ -6,7 +6,14 @@
     NSAssert(self.superview, @"Cell doesn't have a superview!");
     UITableView *tableView = (UITableView *)self.superview;
     NSIndexPath *indexPath = [tableView indexPathForCell:self];
-    [tableView.delegate tableView:tableView didSelectRowAtIndexPath:indexPath];
+
+    if ([tableView.delegate respondsToSelector:@selector(tableView:willSelectRowAtIndexPath:)]) {
+        indexPath = [tableView.delegate tableView:tableView willSelectRowAtIndexPath:indexPath];
+    }
+
+    if (indexPath != nil) {
+        [tableView.delegate tableView:tableView didSelectRowAtIndexPath:indexPath];
+    }
 }
 
 @end
