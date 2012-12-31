004652cac531070b5b3ba4e33ca7402c6fd8de76
Adam Milligan <pair+adam@pivotallabs.com>
2012-12-31 14:18:35 -0500

    Simple example of using subjectAction in a spec

--- Foundation/Spec/Extensions/NSDataSpec+PivotalCore.mm
+++ Foundation/Spec/Extensions/NSDataSpec+PivotalCore.mm
@@ -14,37 +14,37 @@ SPEC_BEGIN(NSData_PivotalCore)
 describe(@"Pivotal Core extensions to NSData ", ^{
     describe(@"initWithSHA1HashOfString:", ^{
         __block NSData *newData;
+        __block NSString *string;
+
+        subjectAction(^{ newData = [[[NSData alloc] initWithSHA1HashOfString:string] autorelease]; });
 
         describe(@"with an empty string", ^{
             beforeEach(^{
-                newData = [[[NSData alloc] initWithSHA1HashOfString:@""] autorelease];
+                string = @"";
             });
 
             it(@"should initialize with the SHA1 hash of an empty string", ^{
-                NSString *description = newData.description;
-                expect(description).to(equal(@"<da39a3ee 5e6b4b0d 3255bfef 95601890 afd80709>"));
+                expect(newData.description).to(equal(@"<da39a3ee 5e6b4b0d 3255bfef 95601890 afd80709>"));
             });
         });
 
         describe(@"with a non-empty string", ^{
             beforeEach(^{
-                newData = [[[NSData alloc] initWithSHA1HashOfString:@"wibble"] autorelease];
+                string = @"wibble";
             });
 
             it(@"should initialize with the correct SHA1 hash data", ^{
-                NSString *description = newData.description;
-                expect(description).to(equal(@"<02e0182a e38f90d1 1be647e3 37665e67 f9243817>"));
+                expect(newData.description).to(equal(@"<02e0182a e38f90d1 1be647e3 37665e67 f9243817>"));
             });
         });
 
         describe(@"with a nil string parameter", ^{
             beforeEach(^{
-                newData = [[[NSData alloc] initWithSHA1HashOfString:nil] autorelease];
+                string = nil;
             });
 
             it(@"should initialize with the SHA1 hash of an empty string", ^{
-                NSString *description = newData.description;
-                expect(description).to(equal(@"<da39a3ee 5e6b4b0d 3255bfef 95601890 afd80709>"));
+                expect(newData.description).to(equal(@"<da39a3ee 5e6b4b0d 3255bfef 95601890 afd80709>"));
             });
         });
     });
