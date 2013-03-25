f30c20b7531c6df18866c496cd265593a7a0bada
pivotal <pivotal@cabrillo.sf.pivotallabs.com>
2013-03-25 16:40:57 -0700

    Throw exception if bar button disabled; support nested button view in bar button
    [#43663711]

--- UIKit/Spec/Extensions/UIBarButtonItemSpec+Spec.mm
+++ UIKit/Spec/Extensions/UIBarButtonItemSpec+Spec.mm
@@ -15,15 +15,29 @@ describe(@"UIBarButtonItemSpec_Spec", ^{
 
     beforeEach(^{
         target = [[Target new] autorelease];
-    });
-
-    it(@"can be 'tapped' programatically", ^{
         barButtonItem = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
                                                                        target:target
                                                                        action:@selector(callMe)] autorelease];
+    });
+
+    it(@"can be 'tapped' programmatically", ^{
         [barButtonItem tap];
         target.wasCalled should be_truthy;
     });
+    
+    it(@"should throw an exception if the bar button item is disabled", ^{
+        barButtonItem.enabled = NO;
+        expect(^{[barButtonItem tap];}).to(raise_exception());
+    });
+    
+    it(@"should delegate down to the custom view if the custom view is a button", ^{
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [button addTarget:target action:@selector(callMe) forControlEvents:UIControlEventTouchUpInside];
+        barButtonItem = [[[UIBarButtonItem alloc] initWithCustomView:button] autorelease];
+        [barButtonItem tap];
+        target.wasCalled should be_truthy;
+    });
+    
 });
 
 SPEC_END
--- UIKit/SpecHelper/Extensions/UIBarButtonItem+Spec.m
+++ UIKit/SpecHelper/Extensions/UIBarButtonItem+Spec.m
@@ -1,4 +1,5 @@
 #import "UIBarButtonItem+Spec.h"
+#import "UIControl+Spec.h"
 #import <objc/message.h>
 
 
@@ -6,9 +7,18 @@
 
 - (void)tap
 {
-    id target = self.target;
-    SEL action = self.action;
-    objc_msgSend(target, action, nil);
+    if (self.enabled == NO) {
+        @throw @"Attempted to tap disabled bar button item";
+    }
+
+    if ([self.customView isKindOfClass:[UIButton class]]) {
+        UIButton *button = (UIButton *)self.customView;
+        [button tap];
+    } else {
+        id target = self.target;
+        SEL action = self.action;
+        objc_msgSend(target, action, nil);
+    }
 }
 
 @end
