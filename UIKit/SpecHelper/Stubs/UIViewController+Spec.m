a402fd94db475c6be827c674049ed969d3ed8f07
Johnathon Britz and Jonathan Barnes <pair+jbritz+jonathan@pivotallabs.com>
2013-06-06 11:22:12 -0700

    Supresses some 6.0 deprecation warnings

--- UIKit/SpecHelper/Stubs/UIViewController+Spec.m
+++ UIKit/SpecHelper/Stubs/UIViewController+Spec.m
@@ -12,7 +12,9 @@ static char PRESENTED_CONTROLLER_KEY;
 #pragma mark - Modals
 
 - (void)presentViewController:(UIViewController *)modalViewController animated:(BOOL)animated completion:(void(^)(void))onComplete {
+    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
     [self presentModalViewController:modalViewController animated:animated];
+    #pragma GCC diagnostic warning "-Wdeprecated-declarations"
     if (onComplete) {
         onComplete();
     }
@@ -38,7 +40,9 @@ static char PRESENTED_CONTROLLER_KEY;
 }
 
 - (void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion {
+    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
     [self dismissModalViewControllerAnimated:flag];
+    #pragma GCC diagnostic warning "-Wdeprecated-declarations"
     if (completion) {
         completion();
     }
