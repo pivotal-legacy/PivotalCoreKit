dc6acf24524977a1e00bf7608613506d544091c5
Pivotal Labs Common Effort Role Account <accounts+github+common@pivotallabs.com>
2013-07-08 15:34:29 -0400

    Update PCKHTTPConnection.h
    PCKHTTPConnection does not actually implement NSURLConnectionDelegate methods, so it should not conform to the NSURLConnectionDelegate protocol, nor should it import the protocol.

--- Foundation/Core/Interfaces/PCKHTTPConnection.h
+++ Foundation/Core/Interfaces/PCKHTTPConnection.h
@@ -1,9 +1,8 @@
 #import <Foundation/Foundation.h>
-#import "NSURLConnectionDelegate.h"
 
 @class PCKHTTPInterface;
 
-@interface PCKHTTPConnection : NSURLConnection <NSURLConnectionDelegate>
+@interface PCKHTTPConnection : NSURLConnection
 
 - (id)initWithHTTPInterface:(PCKHTTPInterface *)interface forRequest:(NSURLRequest *)request andDelegate:(id<NSURLConnectionDelegate>)delegate;
 
