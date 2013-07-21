c98880029d50f5320f0f017b35fc397937eb68f4
Andrew Kitchen <akitchen@pivotallabs.com>
2013-07-21 13:44:10 -0700

    Update Cedar to latest
    Cedar now includes UIKit/CoreGraphics struct equality matchers, so existing code was deleted
    Retaining and continuing to import otherwise empty matcher header for now

--- UIKit/SpecHelper/Matchers/UIKitMatchers.h
+++ UIKit/SpecHelper/Matchers/UIKitMatchers.h
@@ -3,53 +3,11 @@
 #import "ComparatorsBase.h"
 
 namespace Cedar { namespace Matchers { namespace Comparators {
-    template<typename U>
-    bool compare_equal(CGRect const actualValue, const U & expectedValue) {
-        return CGRectEqualToRect(actualValue, expectedValue);
-    }
 
-    template<typename U>
-    bool compare_equal(CGSize const actualValue, const U & expectedValue) {
-        return CGSizeEqualToSize(actualValue, expectedValue);
-    }
-
-    template<typename U>
-    bool compare_equal(CGPoint const actualValue, const U & expectedValue) {
-        return CGPointEqualToPoint(actualValue, expectedValue);
-    }
-
-    template<typename U>
-    bool compare_equal(UIEdgeInsets const actualValue, const U & expectedValue) {
-        return UIEdgeInsetsEqualToEdgeInsets(actualValue, expectedValue);
-    }
-
-    template<typename U>
-    bool compare_equal(CGAffineTransform const actualValue, const U & expectedValue) {
-        return CGAffineTransformEqualToTransform(actualValue, expectedValue);
-    }
 }}}
 
-#import <sstream>
 #import "StringifiersBase.h"
 
 namespace Cedar { namespace Matchers { namespace Stringifiers {
-    inline NSString * string_for(const CGRect value) {
-        return NSStringFromCGRect(value);
-    }
 
-    inline NSString * string_for(const CGSize value) {
-        return NSStringFromCGSize(value);
-    }
-
-    inline NSString * string_for(const CGPoint value) {
-        return NSStringFromCGPoint(value);
-    }
-
-    inline NSString * string_for(const UIEdgeInsets value) {
-        return NSStringFromUIEdgeInsets(value);
-    }
-
-    inline NSString * string_for(const CGAffineTransform value) {
-        return NSStringFromCGAffineTransform(value);
-    }
 }}}
