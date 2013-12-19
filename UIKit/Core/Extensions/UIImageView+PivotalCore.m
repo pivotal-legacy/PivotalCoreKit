6f674bf6ea0fd67f9e244bc5566c43d152e1c23f
Alex Basson <alexbasson@gmail.com>
2013-12-18 21:54:32 -0500

    Add convenience method for creating UIImageViews

---
+++ UIKit/Core/Extensions/UIImageView+PivotalCore.m
@@ -0,0 +1,9 @@
+#import "UIImageView+PivotalCore.h"
+
+@implementation UIImageView (PivotalCore)
+
++ (UIImageView *)imageViewWithImageNamed:(NSString *)imageName {
+    return [[UIImageView alloc] initWithImage:[UIImage imageNamed:imageName]];
+}
+
+@end
