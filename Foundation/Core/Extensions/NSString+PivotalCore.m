6bebabff3390be886383bd54dc72c99f1ec6e9d2
Daniel Shusta <pair+dshusta@pivotallabs.com>
2014-03-03 14:43:54 -0800

    Deprecated PCK base64 methods, calls through to built-in iOS base64 methods now.

--- Foundation/Core/Extensions/NSString+PivotalCore.m
+++ Foundation/Core/Extensions/NSString+PivotalCore.m
@@ -1,51 +1,22 @@
 #import <Foundation/Foundation.h>
 #import "NSString+PivotalCore.h"
 
-static const unsigned char BASE64_DICTIONARY[] = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
-};
-
 @implementation NSString (PivotalCore)
 
-+ (id)stringWithBase64EncodedData:(NSData *)data {
-    return [[[[self class] alloc] initWithBase64EncodedData:data] autorelease];
++ (id)stringWithBase64EncodedData:(NSData *)data DEPRECATED_ATTRIBUTE {
+    if ([data respondsToSelector:@selector(base64EncodedStringWithOptions:)]) {
+        return [data base64EncodedStringWithOptions:0];
+    } else {
+        return [data base64Encoding];
+    }
 }
 
-- (id)initWithBase64EncodedData:(NSData *)data {
-    // Based on standard specified in RFC4648: http://tools.ietf.org/html/rfc4648
-
-    size_t numberOfChunks = [data length] / 3;
-    size_t extraBytes = [data length] % 3;
-
-    size_t resultLength = 4 * numberOfChunks + (extraBytes ? 4 : 0);
-    unsigned char result[resultLength];
-
-    const unsigned char * bytes = [data bytes];
-    for (unsigned char chunkIndex = 0; chunkIndex < numberOfChunks; ++chunkIndex) {
-        const unsigned char * currentChunk = bytes + chunkIndex * 3;
-        result[chunkIndex * 4] = BASE64_DICTIONARY[(*currentChunk & 0xFC) >> 2];
-        result[chunkIndex * 4 + 1] = BASE64_DICTIONARY[(*currentChunk & 0x03) << 4 | ((*(currentChunk + 1) & 0xF0) >> 4)];
-        result[chunkIndex * 4 + 2] = BASE64_DICTIONARY[((*(currentChunk + 1) & 0x0F) << 2) | ((*(currentChunk + 2) & 0xC0) >> 6)];
-        result[chunkIndex * 4 + 3] = BASE64_DICTIONARY[*(currentChunk + 2) & 0x3F];
+- (id)initWithBase64EncodedData:(NSData *)data DEPRECATED_ATTRIBUTE {
+    if ([data respondsToSelector:@selector(base64EncodedStringWithOptions:)]) {
+        return [[data base64EncodedStringWithOptions:0] retain];
+    } else {
+        return [[data base64Encoding] retain];
     }
-
-    if (1 == extraBytes) {
-        result[numberOfChunks * 4] = BASE64_DICTIONARY[(*(bytes + numberOfChunks * 3) & 0xFC) >> 2];
-        result[numberOfChunks * 4 + 1] = BASE64_DICTIONARY[(*(bytes + numberOfChunks * 3) & 0x03) << 4];
-        result[resultLength - 2] = '=';
-        result[resultLength - 1] = '=';
-    } else if (2 == extraBytes) {
-        result[numberOfChunks * 4] = BASE64_DICTIONARY[(*(bytes + numberOfChunks * 3) & 0xFC) >> 2];
-        result[numberOfChunks * 4 + 1] = BASE64_DICTIONARY[(*(bytes + numberOfChunks * 3) & 0x03) << 4 | ((*(bytes + numberOfChunks * 3 + 1) & 0xF0) >> 4)];
-        result[numberOfChunks * 4 + 2] = BASE64_DICTIONARY[((*(bytes + numberOfChunks * 3 + 1) & 0x0F) << 2)];
-        result[resultLength - 1] = '=';
-    }
-
-    return [self initWithBytes:result length:resultLength encoding:NSASCIIStringEncoding];
 }
 
 - (NSString *)stringByAddingPercentEscapesUsingEncoding:(NSStringEncoding)encoding includeAll:(BOOL)includeAll {
