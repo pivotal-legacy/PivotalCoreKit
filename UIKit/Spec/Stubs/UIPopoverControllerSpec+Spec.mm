44881763ddbf9530c152e812a08e356223fd6e22
Daniel Shusta <pair+dshusta@pivotallabs.com>
2014-03-06 10:45:17 -0800

    UIPopoverController stub can be dismissed, queried for visibility, and queried for arrow direction. Added test coverage of all UIPopoverController methods.

--- UIKit/Spec/Stubs/UIPopoverControllerSpec+Spec.mm
+++ UIKit/Spec/Stubs/UIPopoverControllerSpec+Spec.mm
@@ -4,63 +4,190 @@
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
 
+@interface SpecPopoverBackgroundView : UIPopoverBackgroundView
+@end
+
+@implementation SpecPopoverBackgroundView
+@end
+
 SPEC_BEGIN(UIPopoverControllerSpecExtensionsSpec)
 
 describe(@"UIPopoverController (spec extensions)", ^{
     __block UIPopoverController *popoverController;
     __block UIViewController *contentViewController;
-    
+
     afterEach(^{
         stop_spying_on([UIDevice currentDevice]);
     });
-    
+
     beforeEach(^{
         spy_on([UIDevice currentDevice]);
         [UIDevice currentDevice] stub_method(@selector(userInterfaceIdiom)).and_return(UIUserInterfaceIdiomPad);
 
         contentViewController = [[UIViewController alloc] init];
         popoverController = [[UIPopoverController alloc] initWithContentViewController:contentViewController];
-        
+
         popoverController should_not be_nil;
     });
-    
-    describe(@"+currentPopoverController", ^{
+
+    describe(@"stubbed methods", ^{
         describe(@"when the popoverController is presented from rect", ^{
             beforeEach(^{
                 UIView *presentingView = [[UIView alloc] initWithFrame:(CGRect){.size.width = 200, .size.height = 200}];
                 UIWindow *window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
                 [window addSubview:presentingView];
-                
+
                 [popoverController presentPopoverFromRect:CGRectMake(20, 20, 30, 30)
                                                    inView:presentingView
-                                 permittedArrowDirections:UIPopoverArrowDirectionAny
+                                 permittedArrowDirections:UIPopoverArrowDirectionLeft
                                                  animated:YES];
             });
-            
-            it(@"returns the popoverController", ^{
+
+            it(@"+currentPopoverController returns the popoverController", ^{
                 [UIPopoverController currentPopoverController] should be_same_instance_as(popoverController);
             });
+
+            it(@"is visible", ^{
+                popoverController.isPopoverVisible should be_truthy;
+            });
+
+            it(@"popover arrow direction is left", ^{
+                popoverController.popoverArrowDirection should equal(UIPopoverArrowDirectionLeft);
+            });
+
+            describe(@"when a popoverController is then dismissed", ^{
+                beforeEach(^{
+                    [popoverController dismissPopoverAnimated:NO];
+                });
+
+                it(@"+currentPopoverController returns nil", ^{
+                    [UIPopoverController currentPopoverController] should be_nil;
+                });
+
+                it(@"is not visible", ^{
+                    popoverController.isPopoverVisible should be_falsy;
+                });
+            });
         });
-        
+
         describe(@"when the popoverController is presented from a bar button item", ^{
             beforeEach(^{
                 UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] init];
                 UIToolbar *toolbar = [[UIToolbar alloc] init];
                 toolbar.items = @[barButtonItem];
-                
+
                 UIWindow *window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
                 [window addSubview:toolbar];
-                
+
                 [popoverController presentPopoverFromBarButtonItem:barButtonItem
                                           permittedArrowDirections:UIPopoverArrowDirectionAny
                                                           animated:YES];
             });
-            
-            it(@"returns the popoverController", ^{
+
+            it(@"+currentPopoverController returns the popoverController", ^{
                 [UIPopoverController currentPopoverController] should be_same_instance_as(popoverController);
             });
         });
+    });
+
+    describe(@"methods that should continue to work", ^{
+        describe(@"-contentViewController", ^{
+            it(@"should be set to content view controller", ^{
+                popoverController.contentViewController should be_same_instance_as(contentViewController);
             });
+        });
+
+        describe(@"-setContentViewController:animated:", ^{
+            __block UIViewController *newContentViewController;
+
+            beforeEach(^{
+                newContentViewController = [[UIViewController alloc] init];
+
+                [popoverController setContentViewController:newContentViewController animated:YES];
+            });
+
+            it(@"changes contentViewController to newContentViewController", ^{
+                popoverController.contentViewController should be_same_instance_as(newContentViewController);
+            });
+        });
+
+        describe(@"-popoverContentSize", ^{
+            it(@"returns the content size", ^{
+                popoverController.popoverContentSize should equal(CGSizeMake(320, 480));
+            });
+        });
+
+        describe(@"-setPopoverContentSize:animated:", ^{
+            beforeEach(^{
+                [popoverController setPopoverContentSize:CGSizeMake(400, 500) animated:YES];
+            });
+
+            it(@"changes the popoverContentSize", ^{
+                popoverController.popoverContentSize should equal(CGSizeMake(400, 500));
+            });
+        });
+
+        describe(@"setting passthrough views", ^{
+            __block UIView *view;
+            beforeEach(^{
+                view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 30, 400)];
+
+                popoverController.passthroughViews = @[view];
+            });
+
+            it(@"changes the passthroughViews", ^{
+                popoverController.passthroughViews should contain(view);
+            });
+        });
+
+        describe(@"setting the delegate", ^{
+            __block id <UIPopoverControllerDelegate> delegate;
+            beforeEach(^{
+                delegate = nice_fake_for(@protocol(UIPopoverControllerDelegate));
+                popoverController.delegate = delegate;
+            });
+
+            it(@"changes the delegate", ^{
+                popoverController.delegate should be_same_instance_as(delegate);
+            });
+        });
+
+        describe(@"-popoverLayoutMargins", ^{
+            it(@"should returns the default layout margins", ^{
+                popoverController.popoverLayoutMargins should equal(UIEdgeInsetsMake(30, 10, 10, 10));
+            });
+        });
+
+        describe(@"setting the popover layout margins", ^{
+            beforeEach(^{
+                popoverController.popoverLayoutMargins = UIEdgeInsetsMake(5, 10, 15, 20);
+            });
+
+            it(@"changes the popoverLayoutMargins", ^{
+                popoverController.popoverLayoutMargins should equal(UIEdgeInsetsMake(5, 10, 15, 20));
+            });
+        });
+
+        describe(@"setting the popover background view class", ^{
+            beforeEach(^{
+                popoverController.popoverBackgroundViewClass = [SpecPopoverBackgroundView class];
+            });
+
+            it(@"changes the popoverBackgroundViewClass", ^{
+                popoverController.popoverBackgroundViewClass should equal([SpecPopoverBackgroundView class]);
+            });
+        });
+
+        describe(@"setting the background color", ^{
+            beforeEach(^{
+                popoverController.backgroundColor = [UIColor magentaColor];
+            });
+
+            it(@"changes the backgroundColor", ^{
+                popoverController.backgroundColor should equal([UIColor magentaColor]);
+            });
+        });
+    });
 });
 
 SPEC_END
--- UIKit/SpecHelper/Stubs/UIPopoverController+Spec.m
+++ UIKit/SpecHelper/Stubs/UIPopoverController+Spec.m
@@ -1,29 +1,44 @@
-//
-//  UIPopoverController+Spec.m
-//  UIKit
-//
-//  Created by pivotal on 3/4/14.
-//  Copyright (c) 2014 Pivotal Labs. All rights reserved.
-//
-
 #import "UIPopoverController+Spec.h"
 
 @implementation UIPopoverController (Spec)
-
-static UIPopoverController *popoverController;
+static UIPopoverController *currentPopoverController;
+static UIPopoverArrowDirection pckArrowDirection;
 
 + (instancetype)currentPopoverController {
-    return popoverController;
+    return currentPopoverController;
 }
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
 
-// -presentPopoverFromBarButtonItem:permittedArrowDirections:animated: calls through to this method
-- (void)presentPopoverFromRect:(CGRect)rect inView:(UIView *)view permittedArrowDirections:(UIPopoverArrowDirection)arrowDirections animated:(BOOL)animated {
-    popoverController = self;
+- (void)dealloc {
+    // [super dealloc] will raise an exception if -isPopoverVisible is YES when deallocating
+    if (self == currentPopoverController) {
+        currentPopoverController = nil;
+    }
 }
 
-#pragma clang diagnostic pop 
+// -presentPopoverFromBarButtonItem:permittedArrowDirections:animated: calls through to this method
+- (void)presentPopoverFromRect:(CGRect)rect inView:(UIView *)view permittedArrowDirections:(UIPopoverArrowDirection)arrowDirections animated:(BOOL)animated {
+    currentPopoverController = self;
+
+    pckArrowDirection = arrowDirections;
+}
+
+- (void)dismissPopoverAnimated:(BOOL)animated {
+    if (self == currentPopoverController) {
+        currentPopoverController = nil;
+    }
+}
+
+- (BOOL)isPopoverVisible {
+    return (self == currentPopoverController);
+}
+
+- (UIPopoverArrowDirection)popoverArrowDirection {
+    return pckArrowDirection;
+}
+
+#pragma clang diagnostic pop
 
 @end
