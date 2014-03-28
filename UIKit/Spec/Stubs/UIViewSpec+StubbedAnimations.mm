8f07c94eac858bf4252a864198dd0ddadc78d459
Joe Masilotti and Nick Monje <pair+jmasilotti+nmonje@pivotallabs.com>
2014-03-28 11:12:23 -0400

    Add missing stubbed animation method.
    Also backfills tests for `animateWithDuration:animations:` and removes
    whitespace.

--- UIKit/Spec/Stubs/UIViewSpec+StubbedAnimations.mm
+++ UIKit/Spec/Stubs/UIViewSpec+StubbedAnimations.mm
@@ -9,31 +9,90 @@ SPEC_BEGIN(UIView_StubbedAnimations)
 describe(@"UIView+StubbedAnimation", ^{
     __block BOOL animationBlockCalled;
     __block BOOL completionBlockCalled;
+
     beforeEach(^{
         animationBlockCalled = NO;
         completionBlockCalled = NO;
     });
 
+    describe(@"+animateWithDuration:animations:", ^{
+        beforeEach(^{
+            [UIView animateWithDuration:0.666 animations:^{
+                animationBlockCalled = YES;
+            }];
+        });
+
+        it(@"should call the animation block", ^{
+            animationBlockCalled should be_truthy;
+        });
+
+        it(@"should remember the duration", ^{
+            [UIView lastAnimationDuration] should be_close_to(.666);
+        });
+    });
+
     describe(@"+animateWithDuration:animations:completion:", ^{
-        subjectAction(^{
+        beforeEach(^{
             [UIView animateWithDuration:0.666 animations:^{
                 animationBlockCalled = YES;
             } completion:^(BOOL finished) {
                 completionBlockCalled = YES;
             }];
         });
-        
+
         it(@"should call the animation block", ^{
             animationBlockCalled should be_truthy;
         });
-        
+
         it(@"should call the completion block", ^{
             completionBlockCalled should be_truthy;
+        });
+
+        it(@"should remember the duration", ^{
+            [UIView lastAnimationDuration] should be_close_to(.666);
+        });
+    });
+
+    describe(@"+animateWithDuration:delay:usingSpringWithDamping:initialSpringVelocity:options:animations:completion:", ^{
+        beforeEach(^{
+            [UIView animateWithDuration:0.666 delay:10 usingSpringWithDamping:176 initialSpringVelocity:117 options:UIViewAnimationOptionTransitionCrossDissolve animations:^{
+                animationBlockCalled = YES;
+            } completion:^(BOOL finished) {
+                completionBlockCalled = YES;
+            }];
+        });
+
+        it(@"should call the animation block", ^{
+            animationBlockCalled should be_truthy;
+        });
+
+        it(@"should call the completion block", ^{
+            completionBlockCalled should be_truthy;
+        });
+
+        it(@"should remember the damping ratio", ^{
+            [UIView lastAnimationSpringWithDamping] should be_close_to(176);
+        });
+
+        it(@"should remember the initial spring velocity", ^{
+            [UIView lastAnimationInitialSpringVelocity] should be_close_to(117);
+        });
+
+        it(@"should remember the duration", ^{
+            [UIView lastAnimationDuration] should be_close_to(.666);
+        });
+
+        it(@"should remember the delay", ^{
+            [UIView lastAnimationDelay] should be_close_to(10);
+        });
+
+        it(@"should remember the options", ^{
+            [UIView lastAnimationOptions] should equal(UIViewAnimationOptionTransitionCrossDissolve);
         });
     });
 
     describe(@"+animateWithDuration:delay:options:animations:completion:", ^{
-        subjectAction(^{
+        beforeEach(^{
             [UIView animateWithDuration:0.666 delay:10
                                 options:UIViewAnimationOptionTransitionFlipFromBottom
                              animations:^{
