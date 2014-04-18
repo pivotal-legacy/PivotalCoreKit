46548b0c5c4ca60b593c71892cd47f4e87dfbf16
Joe Masilotti <joseph.masilotti@gmail.com>
2014-04-18 09:14:31 -0400

    Adds UIActivityViewController stubs.
    * -activityItems
    * -applicationActivities

---
+++ UIKit/Spec/Stubs/UIActivityViewControllerSpec+Spec.mm
@@ -0,0 +1,54 @@
+#import "SpecHelper.h"
+#import "UIActivityViewController+Spec.h"
+
+@interface CustomActivity : UIActivity
+@end
+
+@implementation CustomActivity
+- (NSString *)activityTitle {
+    return @"Custom Activity";
+}
+@end
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(UIActivityViewControllerSpecExtensionsSpec)
+
+describe(@"UIActivityViewController (spec extensions)", ^{
+    __block UIActivityViewController *activityViewController;
+
+    beforeEach(^{
+        NSArray *activityItems = @[ @"activity item 1", [NSURL URLWithString:@"github.com/pivotal/PivotalCoreKit"] ];
+        CustomActivity *activity = [[CustomActivity alloc] init];
+        activityViewController = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:@[ activity ]];
+    });
+
+    describe(@"-activityItems", ^{
+        it(@"should return the set activities", ^{
+            activityViewController.activityItems should equal(@[ @"activity item 1", [NSURL URLWithString:@"github.com/pivotal/PivotalCoreKit"] ]);
+        });
+    });
+
+    describe(@"-applicationActivites", ^{
+        it(@"should return the set application activities", ^{
+            activityViewController.applicationActivities.count should equal(1);
+            [activityViewController.applicationActivities.lastObject activityTitle] should equal(@"Custom Activity");
+        });
+    });
+
+    describe(@"accessing a modally presented activity view controller", ^{
+        __block UIViewController *presentingViewController;
+
+        beforeEach(^{
+            presentingViewController = [[UIViewController alloc] init];
+            [presentingViewController presentViewController:activityViewController animated:NO completion:nil];
+        });
+
+        it(@"should return the set values", ^{
+            [(UIActivityViewController *)presentingViewController.presentedViewController activityItems] should equal(@[ @"activity item 1", [NSURL URLWithString:@"github.com/pivotal/PivotalCoreKit"] ]);
+        });
+    });
+});
+
+SPEC_END
---
+++ UIKit/SpecHelper/Stubs/UIActivityViewController+Spec.m
@@ -0,0 +1,59 @@
+#import "UIActivityViewController+Spec.h"
+#import <objc/objc-runtime.h>
+
+static char const * const kActivityItemsKey = "activityItemsKey";
+static char const * const kApplicationActivitesKey = "applicationActivitesKey";
+
+@interface UIActivityViewController (SpecPrivate)
+- (instancetype)original_initWithActivityItems:(NSArray *)activityItems applicationActivities:(NSArray *)applicationActivities;
+@end
+
+@implementation UIActivityViewController (Spec)
+
+#pragma mark - TODO Refactor to use code in Foundation project
++ (void)redirectSelector:(SEL)originalSelector to:(SEL)newSelector andRenameItTo:(SEL)renamedSelector {
+    [self redirectSelector:originalSelector
+                      forClass:[self class]
+                            to:newSelector
+                 andRenameItTo:renamedSelector];
+}
+
++ (void)redirectSelector:(SEL)originalSelector forClass:(Class)klass to:(SEL)newSelector andRenameItTo:(SEL)renamedSelector {
+    if ([klass instancesRespondToSelector:renamedSelector]) {
+        return;
+    }
+
+    Method originalMethod = class_getInstanceMethod(klass, originalSelector);
+    class_addMethod(klass, renamedSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+
+    Method newMethod = class_getInstanceMethod(klass, newSelector);
+    class_replaceMethod(klass, originalSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod));
+}
+
++ (void)load
+{
+    [self redirectSelector:@selector(initWithActivityItems:applicationActivities:)
+                        to:@selector(_initWithActivityItems:applicationActivities:)
+             andRenameItTo:@selector(original_initWithActivityItems:applicationActivities:)];
+}
+
+- (instancetype)_initWithActivityItems:(NSArray *)activityItems applicationActivities:(NSArray *)applicationActivities
+{
+    if ((self = [self original_initWithActivityItems:activityItems applicationActivities:applicationActivities])) {
+        objc_setAssociatedObject(self, kActivityItemsKey, activityItems, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        objc_setAssociatedObject(self, kApplicationActivitesKey, applicationActivities, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return self;
+}
+
+- (NSArray *)activityItems
+{
+    return objc_getAssociatedObject(self, kActivityItemsKey);
+}
+
+- (NSArray *)applicationActivities
+{
+    return objc_getAssociatedObject(self, kApplicationActivitesKey);
+}
+
+@end
