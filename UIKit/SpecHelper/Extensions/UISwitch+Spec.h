8b1de17cab58bdc570ce7e4979097b276e9f2bc5
Alex Basson <alexbasson@gmail.com>
2014-05-16 23:14:04 -0400

    Add support for toggling UISwitch in specs

---
+++ UIKit/SpecHelper/Extensions/UISwitch+Spec.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface UISwitch (Spec)
+
+- (void)toggle;
+- (void)toggleToOn:(BOOL)on;
+
+@end
---
+++ UIKit/SpecHelper/Extensions/UISwitch+Spec.m
@@ -0,0 +1,27 @@
+#import "UISwitch+Spec.h"
+
+@implementation UISwitch (Spec)
+
+- (void)toggle {
+    if (self.hidden) {
+        [[NSException exceptionWithName:@"Untappable" reason:@"Can't toggle an invisible switch" userInfo:nil] raise];
+    }
+    if (!self.isEnabled) {
+        [[NSException exceptionWithName:@"Untappable" reason:@"Can't toggle a disabled switch" userInfo:nil] raise];
+    }
+    self.on = !self.on;
+    [self sendActionsForControlEvents:UIControlEventValueChanged];
+}
+
+- (void)toggleToOn:(BOOL)on {
+    if (self.hidden) {
+        [[NSException exceptionWithName:@"Untappable" reason:@"Can't toggle an invisible switch" userInfo:nil] raise];
+    }
+    if (!self.isEnabled) {
+        [[NSException exceptionWithName:@"Untappable" reason:@"Can't toggle a disabled switch" userInfo:nil] raise];
+    }
+    self.on = on;
+    [self sendActionsForControlEvents:UIControlEventValueChanged];
+}
+
+@end
