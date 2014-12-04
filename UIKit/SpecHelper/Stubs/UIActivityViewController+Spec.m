fb875d49965467bc82b1da03a598ba7bf4560d2b
Andrew Kitchen and Kurtis Seebaldt <akitchen+kseebaldt@pivotal.io>
2014-12-04 15:32:18 -0800

    Fixes UIActivityViewController+Spec to not override its load method, and use PCKMethodRedirector
    This change is also needed for CocoaPods users, since CocoaPods installations would have complained about the undeclared selector

--- UIKit/SpecHelper/Stubs/UIActivityViewController+Spec.m
+++ UIKit/SpecHelper/Stubs/UIActivityViewController+Spec.m
@@ -1,4 +1,5 @@
 #import "UIActivityViewController+Spec.h"
+#import "PCKMethodRedirector.h"
 #import <objc/runtime.h>
 
 static char kActivityItemsKey;
@@ -6,38 +7,25 @@ static char kApplicationActivitesKey;
 
 @interface UIActivityViewController (SpecPrivate)
 - (instancetype)original_initWithActivityItems:(NSArray *)activityItems applicationActivities:(NSArray *)applicationActivities NS_RETURNS_RETAINED;
+- (instancetype)_initWithActivityItems:(NSArray *)activityItems applicationActivities:(NSArray *)applicationActivities __attribute__((objc_method_family(init)));
+@end
+
+@interface UIActivityViewControllerLoader : NSObject
+@end
+
+@implementation UIActivityViewControllerLoader
++ (void)load {
+    [PCKMethodRedirector redirectSelector:@selector(initWithActivityItems:applicationActivities:)
+                                 forClass:[UIActivityViewController class]
+                                       to:@selector(_initWithActivityItems:applicationActivities:)
+                            andRenameItTo:@selector(original_initWithActivityItems:applicationActivities:)];
+}
 @end
 
 @implementation UIActivityViewController (Spec)
 
-#pragma mark - TODO Refactor to use code in Foundation project
-+ (void)redirectSelector:(SEL)originalSelector to:(SEL)newSelector andRenameItTo:(SEL)renamedSelector {
-    [self redirectSelector:originalSelector
-                      forClass:[self class]
-                            to:newSelector
-                 andRenameItTo:renamedSelector];
-}
-
-+ (void)redirectSelector:(SEL)originalSelector forClass:(Class)klass to:(SEL)newSelector andRenameItTo:(SEL)renamedSelector {
-    if ([klass instancesRespondToSelector:renamedSelector]) {
-        return;
-    }
-
-    Method originalMethod = class_getInstanceMethod(klass, originalSelector);
-    class_addMethod(klass, renamedSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-
-    Method newMethod = class_getInstanceMethod(klass, newSelector);
-    class_replaceMethod(klass, originalSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod));
-}
-
-+ (void)load {
-    [self redirectSelector:@selector(initWithActivityItems:applicationActivities:)
-                        to:@selector(_initWithActivityItems:applicationActivities:)
-             andRenameItTo:@selector(original_initWithActivityItems:applicationActivities:)];
-}
-
 - (instancetype)_initWithActivityItems:(NSArray *)activityItems applicationActivities:(NSArray *)applicationActivities {
-    if ((self = [super init])) {
+    if (self = [super init]) {
         objc_setAssociatedObject(self, &kActivityItemsKey, activityItems, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
         objc_setAssociatedObject(self, &kApplicationActivitesKey, applicationActivities, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
