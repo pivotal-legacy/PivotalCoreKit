b9e3b24e45e7d67f5111c7cd09096ad62780519c
Kai Lee and Wiley Kestner <pair+hlee+wkestner@pivotal.io>
2014-12-10 13:53:17 -0800

    Initial WatchKit project commit

---
+++ WatchKit/WatchKit/WatchKit.h
@@ -0,0 +1,13 @@
+//
+//  WatchKit.h
+//  WatchKit
+//
+//  Created by pivotal on 12/10/14.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WatchKit : NSObject
+
+@end
---
+++ WatchKit/WatchKit/WatchKit.m
@@ -0,0 +1,13 @@
+//
+//  WatchKit.m
+//  WatchKit
+//
+//  Created by pivotal on 12/10/14.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import "WatchKit.h"
+
+@implementation WatchKit
+
+@end
