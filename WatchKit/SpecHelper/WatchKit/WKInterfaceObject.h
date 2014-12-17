2ffcd90449fd2c503e0a5d1c8d4e9828fa813519
Kai Lee and Wiley Kestner <hlee+wkestner@pivotal.io>
2014-12-17 10:39:26 -0800

    Add all public WKInterfaceObject setters

--- WatchKit/SpecHelper/WatchKit/WKInterfaceObject.h
+++ WatchKit/SpecHelper/WatchKit/WKInterfaceObject.h
@@ -2,4 +2,11 @@
 
 
 @interface WKInterfaceObject : MessageCapturer
+
+- (void)setHidden:(BOOL)hidden;
+- (void)setAlpha:(CGFloat)alpha;
+
+- (void)setWidth:(CGFloat)width;
+- (void)setHeight:(CGFloat)height;
+
 @end
