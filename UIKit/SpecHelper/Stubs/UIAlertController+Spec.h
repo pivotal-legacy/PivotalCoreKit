06bc86780dd5bd63b5cb254d86e24659defd12e3
Andrew Kitchen <akitchen@pivotal.io>
2015-02-03 10:26:46 -0800

    Removes incorrect macro and drops build-time support for iOS 7

--- UIKit/SpecHelper/Stubs/UIAlertController+Spec.h
+++ UIKit/SpecHelper/Stubs/UIAlertController+Spec.h
@@ -1,10 +1,8 @@
 #import <UIKit/UIKit.h>
 
-#if __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_8_0
 @interface UIAlertController (Spec)
 
 - (void)dismissByTappingCancelButton;
 - (void)dismissByTappingButtonWithTitle:(NSString *)title;
 
 @end
-#endif
