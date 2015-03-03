1f120b9056d6f7e28f5cb2e3c3919616fb0a3584
Tim Jarratt <tjarratt@gmail.com>
2015-03-02 22:10:43 -0800

    Cleanup UIGestureRecognizer+Spec category
    Rather than swizzling methods, which requires reimplementing a fair
    amount of built-in behavior, using the private API here should make
    this more reliable and easier to maintain and reason about when
    debugging specs (because there is no difference to the core methods
    that any gesture recognizer implements).

--- UIKit/Spec/Extensions/UIGestureRecognizerSpec+Spec.mm
+++ UIKit/Spec/Extensions/UIGestureRecognizerSpec+Spec.mm
@@ -155,78 +155,6 @@ describe(@"UIGestureRecognizerSpec", ^{
 
         itShouldBehaveLike(@"triggering a gesture recognizer");
 
-        describe(@"removing targets", ^{
-            __block Target *secondTarget;
-
-            beforeEach(^{
-                secondTarget = [[[Target alloc] init] autorelease];
-                spy_on(secondTarget);
-                [recognizer addTarget:target action:@selector(secondAction)];
-                [recognizer addTarget:secondTarget action:@selector(hello)];
-            });
-
-            context(@"when the target and action are both explicitly specified", ^{
-                beforeEach(^{
-                    [recognizer removeTarget:target action:@selector(hello)];
-                    [recognizer recognize];
-                });
-
-                it(@"should not call the action on the removed target", ^{
-                    target should_not have_received(@selector(hello));
-                });
-
-                it(@"should call the remaining action on the target", ^{
-                    target should have_received(@selector(secondAction));
-                });
-
-                it(@"should call the action on the remaining targets", ^{
-                    secondTarget should have_received(@selector(hello));
-                });
-            });
-
-            context(@"when the target is nil and the action is explicitly specified", ^{
-                beforeEach(^{
-                    [recognizer removeTarget:nil action:@selector(hello)];
-                    [recognizer recognize];
-                });
-
-                it(@"should not call the actions on any targets", ^{
-                    target should_not have_received(@selector(hello));
-                    target should_not have_received(@selector(secondAction));
-                    secondTarget should_not have_received(@selector(hello));
-                });
-            });
-
-            context(@"when the target is explicitly specified and the action is NULL", ^{
-                beforeEach(^{
-                    [recognizer removeTarget:target action:NULL];
-                    [recognizer recognize];
-                });
-
-                it(@"should not call the actions on target", ^{
-                    target should_not have_received(@selector(hello));
-                    target should_not have_received(@selector(secondAction));
-                });
-
-                it(@"should call the action on secondTarget", ^{
-                    secondTarget should have_received(@selector(hello));
-                });
-            });
-
-            context(@"when the target is nil and the action is NULL", ^{
-                beforeEach(^{
-                    [recognizer removeTarget:nil action:NULL];
-                    [recognizer recognize];
-                });
-
-                it(@"should not call the actions on any targets", ^{
-                    target should_not have_received(@selector(hello));
-                    target should_not have_received(@selector(secondAction));
-                    secondTarget should_not have_received(@selector(hello));
-                });
-            });
-        });
-
         describe(@"when initialized without a target or action", ^{
             it(@"should not raise", ^{
                 UITapGestureRecognizer *recognizer = [[[UITapGestureRecognizer alloc] initWithTarget:nil action:nil] autorelease];
@@ -252,47 +180,6 @@ describe(@"UIGestureRecognizerSpec", ^{
         });
 
         itShouldBehaveLike(@"triggering a gesture recognizer");
-
-        describe(@"removing targets", ^{
-            sharedExamplesFor(@"not calling the action on the target", ^(NSDictionary *sharedContext) {
-                it(@"should not call the action on the removed target", ^{
-                    [recognizer recognize];
-                    target should_not have_received(@selector(hello));
-                });
-            });
-
-            context(@"when the target and action are both explicitly specified", ^{
-                beforeEach(^{
-                    [recognizer removeTarget:target action:@selector(hello)];
-                });
-
-                itShouldBehaveLike(@"not calling the action on the target");
-            });
-
-            context(@"when the target is nil and the action is explicitly specified", ^{
-                beforeEach(^{
-                    [recognizer removeTarget:nil action:@selector(hello)];
-                });
-
-                itShouldBehaveLike(@"not calling the action on the target");
-            });
-
-            context(@"when the target is explicitly specified and the action is NULL", ^{
-                beforeEach(^{
-                    [recognizer removeTarget:target action:NULL];
-                });
-
-                itShouldBehaveLike(@"not calling the action on the target");
-            });
-
-            context(@"when the target is nil and the action is NULL", ^{
-                beforeEach(^{
-                    [recognizer removeTarget:nil action:NULL];
-                });
-
-                itShouldBehaveLike(@"not calling the action on the target");
-            });
-        });
     });
 
     context(@"for a segue-triggering gesture recognizer created in a storyboard", ^{
--- UIKit/SpecHelper/Stubs/UIGestureRecognizer+Spec.m
+++ UIKit/SpecHelper/Stubs/UIGestureRecognizer+Spec.m
@@ -8,69 +8,15 @@
 
 static NSString * const kGestureStateKey = @"state";
 
-@interface UIGestureRecognizer (Spec_Private)
+@interface UIGestureRecognizerTarget : NSObject
+- (SEL)_action;
+@end
 
-- (void)addUnswizzledTarget:(id)target action:(SEL)action;
-- (void)addSnoopedTarget:(id)target action:(SEL)action;
-- (void)removeUnswizzledTarget:(id)target action:(SEL)action;
-- (void)removeSnoopedTarget:(id)target action:(SEL)action;
+@interface UIGestureRecognizer (PrivateAPI)
 - (void)_resetGestureRecognizer;
-- (instancetype)initWithoutSwizzledTarget:(id)target action:(SEL)action;
-- (instancetype)initWithSwizzledTarget:(id)target action:(SEL)action;
-
 @end
-
-@interface PCKGestureRecognizerTargetActionPair : NSObject
-
-@property (nonatomic, unsafe_unretained) id target;
-@property (nonatomic) SEL action;
-
-+ (instancetype)targetActionPairWithTarget:(id)target action:(SEL)action;
-
-@end
-
-@implementation PCKGestureRecognizerTargetActionPair
-
-+ (void)load {
-    [PCKMethodRedirector redirectSelector:@selector(addTarget:action:)
-                                 forClass:[UIGestureRecognizer class]
-                                       to:@selector(addSnoopedTarget:action:)
-                            andRenameItTo:@selector(addUnswizzledTarget:action:)];
-
-    [PCKMethodRedirector redirectSelector:@selector(removeTarget:action:)
-                                 forClass:[UIGestureRecognizer class]
-                                       to:@selector(removeSnoopedTarget:action:)
-                            andRenameItTo:@selector(removeUnswizzledTarget:action:)];
-
-    [PCKMethodRedirector redirectSelector:@selector(initWithTarget:action:)
-                                 forClass:[UIGestureRecognizer class]
-                                       to:@selector(initWithSwizzledTarget:action:)
-                            andRenameItTo:@selector(initWithoutSwizzledTarget:action:)];
-}
-
-+ (instancetype)targetActionPairWithTarget:(id)target action:(SEL)action {
-    PCKGestureRecognizerTargetActionPair *pair = [[PCKGestureRecognizerTargetActionPair alloc] init];
-    pair.target = target;
-    pair.action = action;
-    return [pair autorelease];
-}
-
-@end
-
-#pragma mark -
 
 @implementation UIGestureRecognizer (Spec)
-
-- (instancetype)initWithSwizzledTarget:(id)target action:(SEL)action {
-    if (self = [self initWithoutSwizzledTarget:target action:action]) {
-        [self setValue:@(UIGestureRecognizerStatePossible) forKey:kGestureStateKey];
-        if (target && action) {
-            [self addSnoopedTarget:target action:action];
-        }
-    }
-
-    return self;
-}
 
 #pragma mark - Public interface
 - (void)recognize {
@@ -85,8 +31,17 @@ static NSString * const kGestureStateKey = @"state";
     }
 
     [self setValue:@(UIGestureRecognizerStateEnded) forKey:kGestureStateKey];
-    [self.targetsAndActions enumerateObjectsUsingBlock:^(PCKGestureRecognizerTargetActionPair *targetActionPair, NSUInteger index, BOOL *stop) {
-        [targetActionPair.target performSelector:targetActionPair.action withObject:self];
+
+
+    Class targetActionPairClass = NSClassFromString(@"UIGestureRecognizerTarget");
+    Ivar targetIvar = class_getInstanceVariable(targetActionPairClass, "_target");
+    Ivar actionIvar = class_getInstanceVariable(targetActionPairClass, "_action");
+
+    NSArray *targetsAndActions = [self valueForKey:@"_targets"];
+    [targetsAndActions enumerateObjectsUsingBlock:^(UIGestureRecognizerTarget *pair, NSUInteger idx, BOOL *stop) {
+        id target = object_getIvar(pair, targetIvar);
+        SEL action = (SEL)object_getIvar(pair, actionIvar);
+        [target performSelector:action withObject:self];
     }];
 
     [self _resetGestureRecognizer];
@@ -94,45 +49,5 @@ static NSString * const kGestureStateKey = @"state";
 }
 
 + (void)whitelistClassForGestureSnooping:(Class)klass {}
-
-#pragma mark - swizzled methods
-- (void)addSnoopedTarget:(id)target action:(SEL)action {
-    [self addUnswizzledTarget:target action:action];
-    [[self targetsAndActions] addObject:[PCKGestureRecognizerTargetActionPair targetActionPairWithTarget:target action:action]];
-}
-
-- (void)removeSnoopedTarget:(id)target action:(SEL)action {
-    [self removeUnswizzledTarget:target action:action];
-
-    NSMutableArray *targetsAndActions = [self targetsAndActions];
-    __block id unretainedTarget = target;
-
-    if (target) {
-        NSPredicate *targetAndActionPredicate = [NSPredicate predicateWithBlock:^BOOL(PCKGestureRecognizerTargetActionPair *targetActionPair, NSDictionary *bindings) {
-            return unretainedTarget == targetActionPair.target && (action == NULL || action == targetActionPair.action);
-        }];
-        NSArray *matchingTargetsAndActions = [targetsAndActions filteredArrayUsingPredicate:targetAndActionPredicate];
-        [targetsAndActions removeObjectsInArray:matchingTargetsAndActions];
-    } else {
-        [targetsAndActions removeAllObjects];
-    }
-}
-
-#pragma mark - Targets and Actions
-
-static char TARGETS_AND_ACTIONS_KEY;
-
-- (NSMutableArray *)targetsAndActions {
-    NSMutableArray *targetsAndActions = objc_getAssociatedObject(self, &TARGETS_AND_ACTIONS_KEY);
-    if (!targetsAndActions) {
-        targetsAndActions = [[[NSMutableArray alloc] init] autorelease];
-        [self setTargetsAndActions:targetsAndActions];
-    }
-    return targetsAndActions;
-}
-
-- (void)setTargetsAndActions:(NSMutableArray *)targetsAndActions {
-    objc_setAssociatedObject(self, &TARGETS_AND_ACTIONS_KEY, targetsAndActions, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
 
 @end
