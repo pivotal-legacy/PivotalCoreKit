d93e7da2184306c93e8e491ccd183a109565becc
Jason McKee <mckeejm@gmail.com>
2015-04-21 19:58:58 -0400

    add support for WKInterfaceDevice

---
+++ WatchKit/WatchKit/WKInterfaceDevice.h
@@ -0,0 +1,21 @@
+#import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import "PCKMessageCapturer.h"
+
+@class UIImage;
+
+@interface WKInterfaceDevice : PCKMessageCapturer;
+
++ (WKInterfaceDevice *)currentDevice;
+
+- (BOOL)addCachedImage:(UIImage *)image name:(NSString *)name;
+- (BOOL)addCachedImageWithData:(NSData *)imageData name:(NSString *)name;
+- (void)removeCachedImageWithName:(NSString *)name;
+- (void)removeAllCachedImages;
+
+@property(nonatomic,readonly) CGRect screenBounds;
+@property(nonatomic,readonly) CGFloat screenScale;
+@property(nonatomic,readonly,copy) NSString *preferredContentSizeCategory;
+@property(nonatomic,readonly,strong) NSDictionary *cachedImages;
+
+@end
