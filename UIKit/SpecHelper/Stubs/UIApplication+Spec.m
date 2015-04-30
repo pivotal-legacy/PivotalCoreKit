49ea97f54b60e124a8e22cdf6cf3beea250dd55e
Tim Jarratt <tjarratt@gmail.com>
2015-04-30 13:20:39 -0700

    Initialize static variable once, upon class load

--- UIKit/SpecHelper/Stubs/UIApplication+Spec.m
+++ UIKit/SpecHelper/Stubs/UIApplication+Spec.m
@@ -2,29 +2,29 @@
 
 @implementation UIApplication (Spec)
 
-static NSMutableArray *URLs__ = nil;
+static NSMutableArray *URLs = nil;
+
++ (void)load {
+    URLs = [NSMutableArray array];
+}
 
 + (void)afterEach {
     [self reset];
 }
 
 + (void)reset {
-    [URLs__ removeAllObjects];
+    [URLs removeAllObjects];
 }
 
 + (NSURL *)lastOpenedURL {
-    return [URLs__ lastObject];
+    return [URLs lastObject];
 }
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
 - (void)openURL:(NSURL *)url {
-    if (!URLs__) {
-        URLs__ = [[NSMutableArray alloc] init];
-    }
-    [URLs__ addObject:url];
+    [URLs addObject:url];
 }
-
 #pragma clang diagnostic pop
 
 @end
