b7e9c634c0b55bc3cee4f74364f1ee58160f7baf
Aaron Levine and Zac Clark <pair+alevine+zclark@pivotallabs.com>
2015-07-10 16:41:08 -0700

    Adds ability to toggle UIViewController spec stubs
    - [UIViewController +pck_useSpecStubs:] will swap instance methods with
      the signature `pck_methodToStub` with the original method
      `methodToStub` if the original method exists. For example:
    
      `pck_viewWillAppear:` will replace `viewWillAppear:` but `pck_yolo`
      will be ignored.
    - This also makes the stub installation (at least for UIViewController)
      much safer. See apple’s docs for more info:
      https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/CustomizingExistingClasses/CustomizingExistingClasses.html#//apple_ref/doc/uid/TP40011210-CH6-SW4

--- UIKit/SpecHelper/Stubs/UIViewController+Spec.m
+++ UIKit/SpecHelper/Stubs/UIViewController+Spec.m
@@ -1,4 +1,4 @@
-#import <UIKit/UIKit.h>
+#import "UIViewController+Spec.h"
 #import <objc/runtime.h>
 
 #pragma clang diagnostic push
@@ -8,11 +8,56 @@
 static char PRESENTING_CONTROLLER_KEY;
 static char PRESENTED_CONTROLLER_KEY;
 
+
 @implementation UIViewController (Spec)
+
++ (void)load {
+    [self pck_useSpecStubs:YES];
+}
+
++ (void)pck_useSpecStubs:(BOOL)useSpecStubs {
+    unsigned int outCount;
+    Method *methods = class_copyMethodList(self, &outCount);
+    for (int i = 0; i < outCount; ++i) {
+        Method method = methods[i];
+        SEL pckSelector = method_getName(method);
+        NSString *pckSelectorName = NSStringFromSelector(pckSelector);
+
+        if ([pckSelectorName hasPrefix:@"pck_"]) {
+            Method pckMethod = class_getInstanceMethod(self, pckSelector);
+            IMP pckImplementation = method_getImplementation(pckMethod);
+
+            NSString *originalSelectorName = [pckSelectorName substringFromIndex:@"pck_".length];
+            SEL originalSelector = NSSelectorFromString(originalSelectorName);
+
+            Method currentMethod = class_getInstanceMethod(self, originalSelector);
+            if (currentMethod == NULL) { continue; }
+
+            NSString *preservedSelectorName = [@"_pck_preserved_" stringByAppendingString:originalSelectorName];
+            SEL preservedSelector = NSSelectorFromString(preservedSelectorName);
+
+            if (useSpecStubs) {
+                // class_addMethod is used here since it only adds the method if it doesn't already exist, and is a
+                // no-op otherwise. This prevents us from accidentally overwriting the preserved original IMP with a
+                // pck stub
+                class_addMethod(self, preservedSelector, method_getImplementation(currentMethod), method_getTypeEncoding(currentMethod));
+                method_setImplementation(currentMethod, pckImplementation);
+            }
+            else {
+                Method preservedMethod = class_getInstanceMethod(self, preservedSelector);
+                if (preservedMethod == NULL) { continue; }
+
+                IMP preservedImplementation = method_getImplementation(preservedMethod);
+                method_setImplementation(currentMethod, preservedImplementation);
+            }
+        }
+    }
+    free(methods);
+}
 
 #pragma mark - Modals
 
-- (void)presentViewController:(UIViewController *)modalViewController animated:(BOOL)animated completion:(void(^)(void))onComplete {
+- (void)pck_presentViewController:(UIViewController *)modalViewController animated:(BOOL)animated completion:(void(^)(void))onComplete {
     if (!modalViewController) {
         NSString *errorReason = [NSString stringWithFormat:@"Application tried to present a nil modal view controller on target %@", self];
         [[NSException exceptionWithName:NSInvalidArgumentException reason:errorReason userInfo:nil] raise];
@@ -22,17 +67,17 @@ static char PRESENTED_CONTROLLER_KEY;
         [[NSException exceptionWithName:NSInternalInconsistencyException reason:errorReason userInfo:nil] raise];
     }
 
-    self.presentedViewController = modalViewController;
-    modalViewController.presentingViewController = self;
+    [self _pck_setPresentedViewController:modalViewController];
+    [modalViewController _pck_setPresentingViewController:self];
     if (onComplete) {
         onComplete();
     }
 }
 
-- (void)dismissViewControllerAnimated:(BOOL)animated completion:(void (^)(void))completion {
+- (void)pck_dismissViewControllerAnimated:(BOOL)animated completion:(void (^)(void))completion {
     if (self.presentedViewController) {
-        self.presentedViewController.presentingViewController = nil;
-        self.presentedViewController = nil;
+        [self.presentedViewController _pck_setPresentingViewController:nil];
+        [self _pck_setPresentedViewController:nil];
     } else if (self.presentingViewController) {
         [self.presentingViewController dismissModalViewControllerAnimated:YES];
     } else if (self.navigationController) {
@@ -46,43 +91,43 @@ static char PRESENTED_CONTROLLER_KEY;
 
 #pragma mark Deprecated Modal APIs
 
-- (void)presentModalViewController:(UIViewController *)modalViewController animated:(BOOL)animated {
+- (void)pck_presentModalViewController:(UIViewController *)modalViewController animated:(BOOL)animated {
     [self presentViewController:modalViewController animated:animated completion:nil];
 }
 
-- (void)dismissModalViewControllerAnimated:(BOOL)animated {
+- (void)pck_dismissModalViewControllerAnimated:(BOOL)animated {
     [self dismissViewControllerAnimated:animated completion:nil];
 }
 
-- (UIViewController *)modalViewController {
+- (UIViewController *)pck_modalViewController {
     return self.presentedViewController;
 }
 
 #pragma mark Modal Properties
 
-- (void)setPresentedViewController:(UIViewController *)modalViewController {
+- (void)_pck_setPresentedViewController:(UIViewController *)modalViewController {
     objc_setAssociatedObject(self, &PRESENTED_CONTROLLER_KEY, modalViewController, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
-- (UIViewController *)presentedViewController {
+- (UIViewController *)pck_presentedViewController {
     return objc_getAssociatedObject(self, &PRESENTED_CONTROLLER_KEY);
 }
 
-- (void)setPresentingViewController:(UIViewController *)presentingViewController {
+- (void)_pck_setPresentingViewController:(UIViewController *)presentingViewController {
     objc_setAssociatedObject(self, &PRESENTING_CONTROLLER_KEY, presentingViewController, OBJC_ASSOCIATION_ASSIGN);
 }
 
-- (UIViewController *)presentingViewController {
+- (UIViewController *)pck_presentingViewController {
     return objc_getAssociatedObject(self, &PRESENTING_CONTROLLER_KEY);
 }
 
 #pragma mark - Animation
-- (void)animateWithDuration:(NSTimeInterval)duration animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion {
+- (void)pck_animateWithDuration:(NSTimeInterval)duration animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion {
     animations();
     completion(YES);
 }
 
-- (void)transitionFromViewController:(UIViewController *)fromViewController toViewController:(UIViewController *)toViewController duration:(NSTimeInterval)duration options:(UIViewAnimationOptions)options animations:(void (^)(void))animations completion:(void (^)(BOOL))completion {
+- (void)pck_transitionFromViewController:(UIViewController *)fromViewController toViewController:(UIViewController *)toViewController duration:(NSTimeInterval)duration options:(UIViewAnimationOptions)options animations:(void (^)(void))animations completion:(void (^)(BOOL))completion {
     animations();
     completion(YES);
 }
