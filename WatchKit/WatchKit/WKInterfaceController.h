fb57919ab10bb65c4049bf52a0cec06446a4026a
Priyanka Ranjan <pranjan@pivotal.io>
2015-10-13 11:13:01 -0400

    PriyankaR - Added updateUserActivity:userInfo:webpageURL to WKInterfaceController

--- WatchKit/WatchKit/WKInterfaceController.h
+++ WatchKit/WatchKit/WKInterfaceController.h
@@ -77,6 +77,7 @@ typedef enum WKTextInputMode : NSInteger  {
 - (void)clearAllMenuItems;
 
 - (void)updateUserActivity:(NSString *)type userInfo:(NSDictionary *)userInfo;
+- (void)updateUserActivity:(NSString *)type userInfo:(nullable NSDictionary *)userInfo webpageURL:(nullable NSURL *)webpageURL;
 
 + (BOOL)openParentApplication:(NSDictionary *)userInfo reply:(void(^)(NSDictionary *replyInfo, NSError *error)) reply;
 
