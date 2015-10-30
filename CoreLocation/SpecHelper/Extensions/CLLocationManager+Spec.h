05433c7c87ce63092c6ad7a6dbabf24fd9274ae9
Brian Croom <bcroom@pivotal.io>
2015-10-30 13:32:00 -0400

    Add nullability annotations to the CoreLocation project

--- CoreLocation/SpecHelper/Extensions/CLLocationManager+Spec.h
+++ CoreLocation/SpecHelper/Extensions/CLLocationManager+Spec.h
@@ -1,7 +1,11 @@
 #import <CoreLocation/CoreLocation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface CLLocationManager (Spec)
 
 + (void)setAuthorizationStatus:(CLAuthorizationStatus)authorizationStatus;
 
 @end
+
+NS_ASSUME_NONNULL_END
