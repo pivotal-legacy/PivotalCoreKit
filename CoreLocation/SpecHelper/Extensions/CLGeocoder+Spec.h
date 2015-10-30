e64b61709a96eca16c1b6473136fab0dfe98e44a
Brian Croom <bcroom@pivotal.io>
2015-10-30 13:32:00 -0400

    Add nullability annotations to the CoreLocation project

--- CoreLocation/SpecHelper/Extensions/CLGeocoder+Spec.h
+++ CoreLocation/SpecHelper/Extensions/CLGeocoder+Spec.h
@@ -1,12 +1,16 @@
 #import <CoreLocation/CoreLocation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface CLGeocoder (Spec)
 
-@property (nonatomic, readonly) NSString *addressString;
-@property (nonatomic, readonly) CLLocation *location;
+@property (nonatomic, readonly, nullable) NSString *addressString;
+@property (nonatomic, readonly, nullable) CLLocation *location;
 
 - (void)completeGeocodeWithPlacemarks:(NSArray *)placemarks;
 - (void)completeReverseGeocodeWithPlacemarks:(NSArray *)placemarks;
 - (void)completeGeocodeWithError:(NSError *)error;
 
 @end
+
+NS_ASSUME_NONNULL_END
