3073229fe5d5bfd1bf9a86c214c33f42a698f779
Tim Jarratt <tjarratt@gmail.com>
2016-08-21 17:28:03 -0700

    Add UISegmentedControl+Spec to umbrella header
    This is absolutely necessary if you want to use this from a Swift
    context.
    
    Also took the liberty of arranging these alphabetically so it would
    be slightly easier to tell if we are missing one of these in the future.

--- UIKit/SpecHelper/UIKit+PivotalSpecHelper.h
+++ UIKit/SpecHelper/UIKit+PivotalSpecHelper.h
@@ -1,11 +1,12 @@
 #import "UIBarButtonItem+Spec.h"
+#import "UICollectionReusableView+Spec.h"
+#import "UICollectionViewCell+Spec.h"
 #import "UIControl+Spec.h"
 #import "UIImage+Spec.h"
+#import "UISegmentedControl+Spec.h"
+#import "UITabBarController+Spec.h"
 #import "UITableViewCell+Spec.h"
 #import "UIView+Spec.h"
-#import "UITabBarController+Spec.h"
-#import "UICollectionViewCell+Spec.h"
-#import "UICollectionReusableView+Spec.h"
 #import "UIWindow+Spec.h"
 
 #if !TARGET_OS_TV
