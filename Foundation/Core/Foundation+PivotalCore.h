3250432178a857715cabf50a171634ab6c2a8a9e
Andrew Kitchen <akitchen@gmail.com>
2017-02-03 20:49:01 -0800

    Omitting the connection helper and parser headers on watchOS

--- Foundation/Core/Foundation+PivotalCore.h
+++ Foundation/Core/Foundation+PivotalCore.h
@@ -7,6 +7,9 @@
 #import "NSDictionary+QueryString.h"
 #import "NSURL+QueryComponents.h"
 
+#ifdef TARGET_OS_WATCH
+#if !TARGET_OS_WATCH
+
 #import "NSURLConnectionDelegate.h" // For pre-10.7 and pre-iOS5
 #import "PCKHTTPInterface.h"
 
@@ -14,3 +17,6 @@
 #import "PCKResponseParser.h"
 #import "PCKXMLParser.h"
 #import "PCKXMLParserDelegate.h"
+
+#endif
+#endif
